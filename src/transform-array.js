337f944ab87b1274634017fc822cddc6cc9c383e
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-03-03 23:19:48 +0300

    feat: completed task "transform-array"

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) throw Error;
+    let newArr = []
+    for(let i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+            case '--double-next' :
+                if (i + 1 < arr.length) {
+                    newArr.push(arr[i + 1]);
+                }
+                break;
+            case '--double-prev':
+                if (i - 1 >= 0) {
+                    newArr.push(arr[i - 1]);
+                }
+                break;
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                newArr.pop();
+                break;
+            default:
+                newArr.push(arr[i]);
+        }
+    }
+
+    return newArr;
 };
