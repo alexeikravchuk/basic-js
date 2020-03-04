b32b602cd5794e9e10efe1c0f117271668e5bbf4
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-03-04 19:50:10 +0300

    feat: task "extended-repeater" done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,16 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    resultArr = [];
+    for(let i = 0; i < (options.repeatTimes || 1); i++) {
+        resultArr[i] = String(str);
+        if(options.hasOwnProperty('addition')) {
+            additionArr = [];
+            for(let j = 0; j < (options.additionRepeatTimes || 1); j++) {
+                additionArr[j] = String(options.addition);
+            }
+            resultArr[i] += additionArr.join(options.additionSeparator || '|');
+        }
+    }
+    
+    return resultArr.join(options.separator || "+");
 };
   
