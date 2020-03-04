a8e040f282183db3c67cd5d16748bf3d3786f289
Alexei Kravchuk <alexei.kravchuk21@gmail.com>
2020-03-04 17:45:41 +0300

    feat: task "recursive-depth" done

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,9 +1,8 @@
 module.exports = class DepthCalculator {
     calculateDepth = (arr) => {
         if (Array.isArray(arr)) {
-            if (arr.length > 0) {
-                const array = arr.map((item) => this.calculateDepth(item));
-                return 1 + Math.max(...array);
+            if (arr.length) {
+                return 1 + Math.max(...arr.map((item) => this.calculateDepth(item)));
             } else {
                 return 1;
             }
